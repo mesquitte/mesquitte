c5bb68a26b9f58de530aeadf761e8962b8413fdb
diablur <diablur@gmail.com>
2024-10-03 09:09:37 +0800

    remove unused

--- mesquitte-core/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    tonic_build::configure()
-        .compile_well_known_types(true)
-        .compile_protos(&["src/idl/sample.proto"], &["idl"])
-        .expect("failed to build protobuf.");
-}
