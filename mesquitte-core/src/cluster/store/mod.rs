4e0a555b4639209aa103b025ac8614bd5f4a3af7
diablur <diablur@gmail.com>
2025-12-23 14:02:21 +0800

    remove cluster

--- mesquitte-core/src/cluster/store/mod.rs
+++
@@ -1,7 +0,0 @@
-#[cfg(feature = "rocksdb-storage")]
-pub mod rocksdb;
-#[cfg(feature = "rocksdb-storage")]
-pub use rocksdb::{
-    log_store,
-    store::{Request, Response, StateMachineData, StateMachineStore, new},
-};
